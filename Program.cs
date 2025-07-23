5a20ce3d981a583327268ac5780f978c86024879
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-07-23 12:17:22 -0300

    Feat: make task

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("1199999999", "Nokia 2000", "123456789", 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("1198888888", "iPhone 13", "987654321", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
